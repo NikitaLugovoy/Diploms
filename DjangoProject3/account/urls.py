bfc5aa3c9d47d0ed70aaeaf841ddbd997f71cab1
ErmishinNikita <saaligin31@yandex.ru>
2025-05-12 20:48:56 +0300

    111

--- DjangoProject3/account/urls.py
+++ DjangoProject3/account/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('success/', views.success_view, name='success'),
