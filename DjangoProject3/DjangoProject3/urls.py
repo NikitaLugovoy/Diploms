9318be9e23f76dba6a9f2258d2e4f1def7731868
Никита Луговой <lugovoinikitka@yandex.r>
2025-06-22 15:21:01 +0300

    Исправление

--- DjangoProject3/DjangoProject3/urls.py
+++ DjangoProject3/DjangoProject3/urls.py
@@ -18,9 +18,23 @@ from django.conf.urls.static import static
 # from django.contrib import admin
 from django.urls import path
 from django.contrib import admin  # Это правильный импорт для админки Django
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from DjangoProject3 import settings
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Документация API",
+        default_version='v1',
+        description="Описание API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +42,9 @@ urlpatterns = [
     path('body/', include('body.urls')),
     path('chat/', include('chat.urls')),
     path('', include('account.urls')),
+    path('swagger-ui/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
 
 
